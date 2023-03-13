ee737d5aeedd0f927f1814d16abec10f4fd0babc
Joan Rue Queralt <joan.rue.q@gmail.com>
2023-03-13 17:54:19 +0100

    [solver-example] fix reference

--- {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_solver.py
+++ {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_solver.py
@@ -60,8 +60,9 @@ class GradientDescent(pyca.Solver):
 
     References
     ----------
-    [Nesterov] Nesterov, Yurii Evgen'evich. "A method of solving a convex programming problem with convergence rate
-    O\bigl(k^2\bigr)." Doklady Akademii Nauk. Vol. 269. No. 3. Russian Academy of Sciences, 1983.
+    .. [1] [Nesterov] Nesterov, Yurii Evgen'evich.
+           "A method of solving a convex programming problem with convergence rate O\bigl(k^2\bigr)."
+           Doklady Akademii Nauk. Vol. 269. No. 3. Russian Academy of Sciences, 1983.
     """
 
     def __init__(
