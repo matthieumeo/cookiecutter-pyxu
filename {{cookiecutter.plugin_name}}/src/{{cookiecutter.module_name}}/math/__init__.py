15e7ea7e6ad0d40045460a0a17e97eb54957d6dd
Joan Rue Queralt <joan.rue.q@gmail.com>
2023-06-02 14:24:26 +0200

    [plugin-examples] add math plugin - Pycsou compliant eigh solver

--- {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/math/__init__.py
+++ {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/math/__init__.py
@@ -1,16 +1,13 @@
-import numpy as np
-import dask.array as da
 import numba
-
+import numpy as np
 import pycsou.util as pycu
-import pycsou.util.ptype as pyct
 import pycsou.util.deps as pycd
-if pycd.CUPY_ENABLED:
-    import cupy as cp
+import pycsou.util.ptype as pyct
 
 __all__ = ["eigh"]
 
-def eigh(arr: pyct.NDArray, arg_shape: pyct.NDArrayShape):
+
+def eigh(arr: pyct.NDArray, arg_shape: pyct.NDArrayShape, normalize=True):
     r"""
     Batch computation of the eigenvalues and eigenvectors of a batch (:math:`\mathcal{N}`-shaped, where
     :math:`\mathcal{N} = (N_{1}, \cdots, N_{k})`) of complex Hermitian (conjugate symmetric) or a real symmetric
@@ -21,14 +18,15 @@ def eigh(arr: pyct.NDArray, arg_shape: pyct.NDArrayShape):
     Returns two objects, an :math:`\mathcal{N}`-shaped batch of 1-D arrays containing the eigenvalues, and an
     :math:`\mathcal{N}`-shaped batch of 2-D square matrices of the corresponding eigenvectors (in columns).
 
-    Note
-    ----
-    In the current version only works for CPU (due to known issue with GPU https://github.com/numba/numba/issues/6256)
+    Closed form 2x2 from https://hal.science/hal-01501221/document
+    Closed form 3x3 from https://www.wikiwand.com/en/Eigenvalue_algorithm
+
 
     Parameters
     ----------
     arr: (…, M, M) pyct.NDArray
     arg_shape: pyct.NDArrayShape
+    normalize: bool
 
     Returns
     -------
@@ -40,52 +38,371 @@ def eigh(arr: pyct.NDArray, arg_shape: pyct.NDArrayShape):
     assert len(np.unique(arg_shape))
     st_sh = arr.shape[:-1]
     st_size = np.prod(st_sh)
-    arr = arr.reshape(st_size, *arg_shape)
-
+    N = pycd.NDArrayInfo
     xp = pycu.get_array_module(arr)
+
+    if N.from_obj(arr) == N.DASK:
+        chunksize = arr.chunksize
+        arr = arr.reshape(*st_sh, *arg_shape)
+        arr = arr.rechunk(
+            chunks=(*((-1),) * len(st_sh), *((-1),) * len(arg_shape))
+        )
+    else:
+        arr = arr.reshape(st_size, *arg_shape)
 
     w = xp.zeros_like(arr[..., 0])
     v = xp.zeros_like(arr)
 
-    eigh_dispatcher(arr, w, v)
-    w = w.reshape((*st_sh, arg_shape[0]))
+    w, v = eigh_dispatcher(arr, w, v)
+
+    if normalize:
+        v /= xp.linalg.norm(v, axis=-2, keepdims=True)
+
+    w = w.reshape((*st_sh, -1))
     v = v.reshape((*st_sh, -1))
+
+    if N.from_obj(arr) == N.DASK:
+        w = w.rechunk(chunks=(*chunksize[:-1], -1))
+        v = v.rechunk(chunks=(*chunksize[:-1], -1))
     return w, v
 
+
 @numba.guvectorize(
-    [(numba.float32[:, :], numba.float32[:], numba.float32[:, :]),
-     (numba.float64[:, :], numba.float64[:], numba.float64[:, :])],
-    '(m, m) -> (m), (m, m)'
+    [
+        (numba.float32[:, :], numba.float32[:]),
+        (numba.float64[:, :], numba.float64[:]),
+    ],
+    "(m, m) -> (m)",
 )
-def eigh_cpu(arr, w, v):
-    w[:], v[:] = np.linalg.eigh(arr)
+def compute_eigenvalues_2x2(arr, out):
+    d = 4 * arr[0, 1] ** 2 + (arr[0, 0] - arr[1, 1]) ** 2
+    d = d**0.5
+    out[0] = (arr[0, 0] + arr[1, 1] - d) / 2
+    out[1] = (arr[0, 0] + arr[1, 1] + d) / 2
 
-# #Can't use eigh_gpu because guvectorize with target=`cuda` doesn't support multiple outputs yet
-# @numba.guvectorize(
-#     [(numba.float32[:, :], numba.float32[:], numba.float32[:, :]),
-#      (numba.float64[:, :], numba.float64[:], numba.float64[:, :])],
-#     '(m, m) -> (m), (m, m)',
-#     target="cuda"
-# )
-# def eigh_gpu(arr, w, v):
-#     w, v = cp.linalg.eigh(arr)
+    if out[0] > out[1]:
+        out[0], out[1] = out[1], out[0]
 
-# #Can't use eigh_dask because map_blocks doesn't support multiple outputs yet
-# def eigh_dask(arr, w, v):
-#     # w, v = da.apply_gufunc(eigh_dispatcher, '(n, m, m),(n, m),(n, m, m)->(n, m),(n, m, m)', arr , w, v)
-#     w, v = da.map_blocks(eigh_dispatcher, arr, w, v)
-#     return w, v
+
+@numba.guvectorize(
+    [
+        (numba.float32[:, :], numba.float32[:], numba.float32[:, :]),
+        (numba.float64[:, :], numba.float64[:], numba.float64[:, :]),
+    ],
+    "(m, m), (m) -> (m, m)",
+)
+def compute_eigenvectors_2x2(arr, eigval, out):
+    p = out[1, 0] ** 2 + out[0, 1] ** 2
+    if p == 0:
+        # A is diagonal.
+        # First eigenvector
+        out[0, 0] = 1
+        out[1, 0] = 0
+        # Second eigenvector
+        out[0, 1] = 0
+        out[1, 1] = 1
+    else:
+        # First eigenvector
+        out[0, 0] = (eigval[1] - arr[0, 0]) / arr[0, 1]
+        out[1, 0] = 1
+        # Second eigenvector
+        out[0, 1] = (eigval[0] - arr[0, 0]) / arr[0, 1]
+        out[1, 1] = 1
+
+
+@numba.guvectorize(
+    [
+        (numba.float32[:, :], numba.float32[:]),
+        (numba.float64[:, :], numba.float64[:]),
+    ],
+    "(m, m) -> (m)",
+)
+def compute_eigenvalues_3x3(arr, out):
+    a = arr[0, 0]
+    d = arr[0, 1]
+    f = arr[0, 2]
+    b = arr[1, 1]
+    e = arr[1, 2]
+    c = arr[2, 2]
+
+    p1 = d**2 + f**2 + e**2
+
+    if p1 == 0:
+        # A is diagonal.
+        out[0] = a
+        out[1] = b
+        out[2] = c
+    else:
+        q = (a + b + c) / 3  # trace(A) is the sum of all diagonal values
+        p2 = (a - q) ** 2 + (b - q) ** 2 + (c - q) ** 2 + 2 * p1
+        p = p2 / 6
+        p = p**0.5
+        det_b = (
+            (a - q) * ((b - q) * (c - q) - e**2)
+            - d * (d * (c - q) - e * f)
+            + f * (d * e - (b - q) * f)
+        )
+        r = det_b / (2 * p**3)
+
+        # In exact arithmetic for a symmetric matrix -1 <= r <= 1
+        # but computation error can leave it slightly outside this range.
+        if r <= -1:
+            phi = np.pi / 3
+        elif r >= 1:
+            phi = 0
+        else:
+            phi = np.arccos(r) / 3
+
+        # the eigenvalues satisfy eig3 <= eig2 <= eig1
+        out[0] = q + 2 * p * np.cos(phi + (2 * np.pi / 3))
+        out[2] = q + 2 * p * np.cos(phi)
+        out[1] = 3 * q - out[0] - out[2]  # since trace(A) = eig1 + eig2 + eig3
+
+    if out[0] > out[1]:
+        out[0], out[1] = out[1], out[0]
+    if out[1] > out[2]:
+        out[1], out[2] = out[2], out[1]
+    if out[0] > out[1]:
+        out[0], out[1] = out[1], out[0]
+
+
+@numba.guvectorize(
+    [
+        (numba.float32[:, :], numba.float32[:], numba.float32[:, :]),
+        (numba.float64[:, :], numba.float64[:], numba.float64[:, :]),
+    ],
+    "(m, m), (m) -> (m, m)",
+)
+def compute_eigenvectors_3x3(arr, eigval, out):
+    # Step 4: Compute the eigenvectors
+
+    d = arr[0, 1]
+    f = arr[0, 2]
+    b = arr[1, 1]
+    e = arr[1, 2]
+    c = arr[2, 2]
+
+    p1 = d**2 + f**2 + e**2
+    if p1 == 0:
+        # A is diagonal.
+        out[0, 0] = 1
+        out[0, 1] = 0
+        out[0, 2] = 0
+        out[1, 0] = 0
+        out[1, 1] = 1
+        out[1, 2] = 0
+        out[2, 0] = 0
+        out[2, 1] = 0
+        out[2, 2] = 1
+    else:
+        m1 = (d * (c - eigval[0]) - e * f) / (f * (b - eigval[0]) - d * e)
+        m2 = (d * (c - eigval[1]) - e * f) / (f * (b - eigval[1]) - d * e)
+        m3 = (d * (c - eigval[2]) - e * f) / (f * (b - eigval[2]) - d * e)
+
+        out[0, 0] = (eigval[0] - c - e * m1) / f
+        out[0, 1] = (eigval[1] - c - e * m2) / f
+        out[0, 2] = (eigval[2] - c - e * m3) / f
+
+        out[1, 0] = m1
+        out[1, 1] = m2
+        out[1, 2] = m3
+
+        out[2, 0] = 1
+        out[2, 1] = 1
+        out[2, 2] = 1
+
+
+@numba.guvectorize(
+    [
+        (numba.float32[:, :], numba.float32[:]),
+        (numba.float64[:, :], numba.float64[:]),
+    ],
+    "(m, m) -> (m)",
+    target="cuda",
+)
+def compute_eigenvalues_2x2_gpu(arr, out):
+    d = 4 * arr[0, 1] ** 2 + (arr[0, 0] - arr[1, 1]) ** 2
+    d = d**0.5
+    out[0] = (arr[0, 0] + arr[1, 1] - d) / 2
+    out[1] = (arr[0, 0] + arr[1, 1] + d) / 2
+
+    if out[0] > out[1]:
+        out[0], out[1] = out[1], out[0]
+
+
+@numba.guvectorize(
+    [
+        (numba.float32[:, :], numba.float32[:], numba.float32[:, :]),
+        (numba.float64[:, :], numba.float64[:], numba.float64[:, :]),
+    ],
+    "(m, m), (m) -> (m, m)",
+    target="cuda",
+)
+def compute_eigenvectors_2x2_gpu(arr, eigval, out):
+    p = arr[0, 1] ** 2 + arr[1, 0] ** 2
+    if p == 0:
+        # A is diagonal.
+        # First eigenvector
+        out[0, 0] = 1
+        out[1, 0] = 0
+        # Second eigenvector
+        out[0, 1] = 0
+        out[1, 1] = 1
+    else:
+        # First eigenvector
+        out[0, 0] = (eigval[1] - arr[0, 0]) / arr[0, 1]
+        out[1, 0] = 1
+        # Second eigenvector
+        out[0, 1] = (eigval[0] - arr[0, 0]) / arr[0, 1]
+        out[1, 1] = 1
+
+
+@numba.guvectorize(
+    [
+        (numba.float32[:, :], numba.float32[:]),
+        (numba.float64[:, :], numba.float64[:]),
+    ],
+    "(m, m) -> (m)",
+    target="cuda",
+)
+def compute_eigenvalues_3x3_gpu(arr, out):
+    a = arr[0, 0]
+    d = arr[0, 1]
+    f = arr[0, 2]
+    b = arr[1, 1]
+    e = arr[1, 2]
+    c = arr[2, 2]
+
+    p1 = d**2 + f**2 + e**2
+
+    if p1 == 0:
+        # A is diagonal.
+        out[0] = a
+        out[1] = b
+        out[2] = c
+    else:
+        q = (a + b + c) / 3
+        p2 = (a - q) ** 2 + (b - q) ** 2 + (c - q) ** 2 + 2 * p1
+        p = p2 / 6
+        p = p**0.5
+        det_b = (
+            (a - q) * ((b - q) * (c - q) - e**2)
+            - d * (d * (c - q) - e * f)
+            + f * (d * e - (b - q) * f)
+        )
+        r = det_b / (2 * p**3)
+
+        # In exact arithmetic for a symmetric matrix -1 <= r <= 1
+        # but computation error can leave it slightly outside this range.
+        if r <= -1:
+            phi = np.pi / 3
+        elif r >= 1:
+            phi = 0
+        else:
+            phi = np.arccos(r) / 3
+
+        # the eigenvalues satisfy eig3 <= eig2 <= eig1
+        out[0] = q + 2 * p * np.cos(phi + (2 * np.pi / 3))
+        out[2] = q + 2 * p * np.cos(phi)
+        out[1] = 3 * q - out[0] - out[2]  # since trace(A) = eig1 + eig2 + eig3
+
+    if out[0] > out[1]:
+        out[0], out[1] = out[1], out[0]
+    if out[1] > out[2]:
+        out[1], out[2] = out[2], out[1]
+    if out[0] > out[1]:
+        out[0], out[1] = out[1], out[0]
+
+
+@numba.guvectorize(
+    [
+        (numba.float32[:, :], numba.float32[:], numba.float32[:, :]),
+        (numba.float64[:, :], numba.float64[:], numba.float64[:, :]),
+    ],
+    "(m, m), (m) -> (m, m)",
+    target="cuda",
+)
+def compute_eigenvectors_3x3_gpu(arr, eigval, out):
+    # Step 4: Compute the eigenvectors
+
+    d = arr[0, 1]
+    f = arr[0, 2]
+    b = arr[1, 1]
+    e = arr[1, 2]
+    c = arr[2, 2]
+
+    p1 = d**2 + f**2 + e**2
+
+    if p1 == 0:
+        # A is diagonal.
+        out[0, 0] = 1
+        out[0, 1] = 0
+        out[0, 2] = 0
+        out[1, 0] = 0
+        out[1, 1] = 1
+        out[1, 2] = 0
+        out[2, 0] = 0
+        out[2, 1] = 0
+        out[2, 2] = 1
+    else:
+        m1 = (d * (c - eigval[0]) - e * f) / (f * (b - eigval[0]) - d * e)
+        m2 = (d * (c - eigval[1]) - e * f) / (f * (b - eigval[1]) - d * e)
+        m3 = (d * (c - eigval[2]) - e * f) / (f * (b - eigval[2]) - d * e)
+
+        out[0, 0] = (eigval[0] - c - e * m1) / f
+        out[0, 1] = (eigval[1] - c - e * m2) / f
+        out[0, 2] = (eigval[2] - c - e * m3) / f
+
+        out[1, 0] = m1
+        out[1, 1] = m2
+        out[1, 2] = m3
+
+        out[2, 0] = 1
+        out[2, 1] = 1
+        out[2, 2] = 1
+
 
 def eigh_dispatcher(arr, w, v):
-    #can't use redirect because @numba.guvectorize doesn't accept keyword args, only positional args.
-
     xp = pycu.get_array_module(arr)
-    if xp == np:
-        w, v = eigh_cpu(arr, w, v)
-    # elif xp == cp:
-    #     w, v = eigh_gpu(arr, w, v)
-    # elif xp == da:
-    #     w, v = eigh_dask(arr, w, v)
+    N = pycd.NDArrayInfo
+    d = arr.shape[-1]
+    if N.from_obj(arr) == N.CUPY:
+        if d == 2:
+            w = compute_eigenvalues_2x2_gpu(arr)
+            v = compute_eigenvectors_2x2_gpu(arr, w)
+        elif d == 3:
+            w = compute_eigenvalues_3x3_gpu(arr)
+            v = compute_eigenvectors_3x3_gpu(arr, w)
+        else:
+            w, v = xp.linalg.eigh(arr)
+    elif N.from_obj(arr) in [N.NUMPY, N.DASK]:
+        if d == 2:
+            w = compute_eigenvalues_2x2(arr)
+            v = compute_eigenvectors_2x2(arr, w)
+        elif d == 3:
+            w = compute_eigenvalues_3x3(arr)
+            v = compute_eigenvectors_3x3(arr, w)
+        else:
+            if N.from_obj(arr) == N.NUMPY:
+                w, v = xp.linalg.eigh(arr)
+            if N.from_obj(arr) == N.DASK:
+                meta = np.array([], dtype=arr.dtype)[
+                    (np.newaxis,) * (arr.ndim - 1)
+                ]
+                tuple_array = arr.map_blocks(
+                    np.linalg.eigh, dtype=arr.dtype, meta=meta
+                )
+                tuple_array = tuple_array.persist()
+                get_w = lambda x: x[0][..., np.newaxis]
+                get_v = lambda x: x[1]
+                w = tuple_array.map_blocks(
+                    get_w,
+                    dtype=arr.dtype,
+                    meta=np.array([], dtype=arr.dtype)[
+                        (np.newaxis,) * (arr.ndim - 2)
+                    ],
+                )[..., 0]
+                v = tuple_array.map_blocks(get_v, dtype=arr.dtype, meta=meta)
     else:
         raise NotImplementedError
     return w, v
--- {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}_tests/test_math.py
+++ {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}_tests/test_math.py
@@ -1,17 +1,93 @@
 import numpy as np
+import pycsou.runtime as pycrt
+import pycsou.util.deps as pycd
+import pytest
 
 from {{cookiecutter.module_name}} import eigh
 
 
-# tmp_path is a pytest fixture
-def test_math():
+def allclose(x, y, xp, sign_matters=True):
+    if sign_matters:
+        return xp.allclose(x, y)
+    else:
+        return xp.allclose(xp.abs(x), xp.abs(y))
+
+
+@pytest.fixture(params=pycd.NDArrayInfo)
+def ndi(request):
+    ndi_ = request.param
+    if ndi_.module() is None:
+        pytest.skip(f"{ndi_} unsupported on this machine.")
+    return ndi_
+
+
+@pytest.fixture(params=pycrt.Width)
+def width(request):
+    return request.param
+
+
+@pytest.fixture(
+    params=[
+        # dim, #scheme
+        (2, "FULL"),
+        (2, "DIAG"),
+        (3, "FULL"),
+        (3, "DIAG"),
+        (5, "FULL"),
+    ]
+)
+def params(request):
+    return request.param
+
+
+@pytest.fixture
+def arg_shape(params):
+    return params[0], params[0]
+
+
+@pytest.fixture
+def scheme(params):
+    return params[1]
+
+
+@pytest.fixture
+def batch_shape():
+    return 3, 2
+
+
+@pytest.fixture(params=[0, 1, 2, 3, 4])
+def seed(request):
+    return request.param
+
+
+@pytest.fixture
+def data(arg_shape, batch_shape, seed, ndi, width, scheme):
+    # Make a batch of real & symmetric input matrices (PSD)
     rng = np.random.default_rng(seed=0)
-    arg_shape = (5, 5)
-    batch_shape = (3, 2)
     input_shape = batch_shape + arg_shape
-    x = rng.normal(size=input_shape)
-    x += x.swapaxes(2,3)
-    w, v = eigh(x.reshape(*batch_shape, -1), arg_shape=arg_shape)
+    if scheme == "FULL":
+        x = rng.normal(size=input_shape)
+        x += x.swapaxes(2, 3)
+    elif scheme == "DIAG":
+        x = rng.normal(size=input_shape)
+        mask = np.tile((np.eye(arg_shape[0])), [*batch_shape, 1, 1])
+        x *= mask
+
     w_np, v_np = np.linalg.eigh(x)
-    assert np.allclose(v.ravel(), v_np.ravel())
-    assert np.allclose(w.ravel(), w_np.ravel())
+    if scheme == "DIAG":
+        v_np = mask
+
+    xp = ndi.module()
+    x = x.reshape(*batch_shape, -1)
+    w_np = w_np.reshape(*batch_shape, -1)
+    v_np = v_np.reshape(*batch_shape, -1)
+    return {"in_": xp.array(x), "out_gt": (xp.array(w_np), xp.array(v_np))}
+
+
+def test_math(data, arg_shape, ndi):
+    x = data["in_"]
+    w_np, v_np = data["out_gt"]
+    w, v = eigh(x, arg_shape=arg_shape)
+    xp = ndi.module()
+    assert allclose(v, v_np, xp, False)
+    assert allclose(w, w_np, xp)
